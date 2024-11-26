a799da252ab898b7b2dce7a4a8c7c6ba1a0d999a
bibinsivanandh25 <bibinsivanandh25@gmail.com>
2024-11-26 08:23:06 +0530

    client side code changes

--- src/services/newsService.js
+++ src/services/newsService.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
+axios.defaults.withCredentials = true;
+
 const BASE_URL = import.meta.env.VITE_BASE_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const fetchForxNews = async (query) => {
   const searchQuery = query || 'forex';
@@ -9,15 +12,13 @@ export const fetchForxNews = async (query) => {
     const { data } = await axios.get(BASE_URL, {
       params: {
         query: searchQuery,
+        apiKey: API_KEY,
         language: 'en',
       },
     });
     return data.articles || [];
   } catch (error) {
-    console.error(
-      'Error fetching news:',
-      error.response ? error.response.data : error.message
-    );
+    console.error('Error fetching news:', error);
     return [];
   }
 };
